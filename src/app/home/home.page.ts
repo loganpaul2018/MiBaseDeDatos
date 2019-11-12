11d05935e6d63cfb525d16515f344b7e88d0a509
Eloy <alum.eloy.garcia.sanchez@ieslosremedios.org>
2019-11-12 12:27:45 +0100

    Commit 1

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,70 @@
 import { Component } from '@angular/core';
-
+import { FirestoreService } from '../firestore.service';
+import { Tarea } from '../tarea';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  tareaEditando: Tarea;
 
-  constructor() {}
+  arrayColeccionTareas: any = [{
+    id: "",
+    data: {} as Tarea
+   }];
 
+  constructor(private firestoreService: FirestoreService) {
+        // Crear una tarea vacía
+        this.tareaEditando = {} as Tarea;
+        this.obtenerListaTareas();
+  }
+  clicBotonInsertar() {
+    this.firestoreService.insertar("tareas", this.tareaEditando).then(() => {
+      console.log('Tarea creada correctamente!');
+      this.tareaEditando= {} as Tarea;
+    }, (error) => {
+      console.error(error);
+    });
+  }
+
+  obtenerListaTareas(){
+    this.firestoreService.consultar("tareas").subscribe((resultadoConsultaTareas) => {
+      this.arrayColeccionTareas = [];
+      resultadoConsultaTareas.forEach((datosTarea: any) => {
+        this.arrayColeccionTareas.push({
+          id: datosTarea.payload.doc.id,
+          data: datosTarea.payload.doc.data()
+        });
+      })
+    });
+  }
+//
+  idTareaSelec: string;
+
+  selecTarea(tareaSelec) {
+    console.log("Tarea seleccionada: ");
+    console.log(tareaSelec);
+    this.idTareaSelec = tareaSelec.id;
+    this.tareaEditando.titulo = tareaSelec.data.titulo;
+    this.tareaEditando.descripcion = tareaSelec.data.descripcion;
+  }
+
+  clicBotonBorrar() {
+    this.firestoreService.borrar("tareas", this.idTareaSelec).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaTareas();
+      // Limpiar datos de pantalla
+      this.tareaEditando = {} as Tarea;
+    })
+  }
+
+  clicBotonModificar() {
+    this.firestoreService.actualizar("tareas", this.idTareaSelec, this.tareaEditando).then(() => {
+      // Actualizar la lista completa
+      this.obtenerListaTareas();
+      // Limpiar datos de pantalla
+      this.tareaEditando = {} as Tarea;
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBznxh5p5Z5molSYPixOzhxOVqj616mM9I",
+    authDomain: "mibasededatos-bcf7d.firebaseapp.com",
+    databaseURL: "https://mibasededatos-bcf7d.firebaseio.com",
+    projectId: "mibasededatos-bcf7d",
+    storageBucket: "mibasededatos-bcf7d.appspot.com",
+    messagingSenderId: "334388003900",
+    appId: "1:334388003900:web:3e09db717214a39712ef39",
+    measurementId: "G-ZXWF1M6HZ9"
+  }
 };
 
 /*
